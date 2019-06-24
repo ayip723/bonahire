56a7626a21daafe752b663f01962e8266fcfbb81
Arthur Yip <arthuryip723@gmail.com>
2019-06-23 22:17:10 -0400

    added user model

--- db/migrate/20190617181935_create_applications.rb
+++ db/migrate/20190617181935_create_applications.rb
@@ -1,7 +1,9 @@
 class CreateApplications < ActiveRecord::Migration[5.2]
   def change
     create_table :applications do |t|
-
+      t.integer :job_id
+      t.integer :user_id
+      t.text :comment
       t.timestamps
     end
   end
--- db/migrate/20190617181947_create_movings.rb
+++ db/migrate/20190617181947_create_movings.rb
@@ -1,7 +1,8 @@
 class CreateMovings < ActiveRecord::Migration[5.2]
   def change
     create_table :movings do |t|
-
+      t.integer :stage_id
+      t.text :comment
       t.timestamps
     end
   end
