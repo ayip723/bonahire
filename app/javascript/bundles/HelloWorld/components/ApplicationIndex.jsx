b9ed6ab1db956a205dc721787bb684226b4a75c2
Arthur Yip <arthuryip723@gmail.com>
2019-08-18 12:41:25 -0400

    added my applications

---
+++ app/javascript/bundles/HelloWorld/components/ApplicationIndex.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ApplicationIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (<div>My applications</div>);
+  }
+}
---
+++ app/javascript/bundles/HelloWorld/containers/ApplicationIndexContainer.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+
+import ApplicationIndex from '../components/ApplicationIndex';
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ApplicationIndex);
