6702a90ed65af5ca3d7765bd690d3ed86fac2e6f
Arthur Yip <arthuryip723@gmail.com>
2019-08-30 08:55:06 -0400

    in progress

---
+++ app/javascript/bundles/HelloWorld/components/MyCompanyJobShow.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// export default () => (
+//   <div>My Company Job Show</div>
+// )
+
+export default class MyCompanyJobShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (<div></div>);
+  }
+}
---
+++ app/javascript/bundles/HelloWorld/containers/MyCompanyJobShowContainer.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+import MyCompanyJobShow from '../components/MyCompanyJobShow';
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyCompanyJobShow);
